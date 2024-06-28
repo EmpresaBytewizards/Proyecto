c1288c7799ac2a9fc65ad652aff7ab3d78022e11
JonathanRaymond2024 <jonathanraymond84@gmail.com>
2024-06-28 18:20:37 -0300

    Bueno estuve trabajando el lograr que las imagenes de (Ofertas del dia) pasen en automatico, esto se logro. ahora lo que estuve trabajando es lograr que las imagenes tomen un mismo tamaño en su contenedor pero no lo e logrado.

--- interfaz/tienda/tiendaWeShop.js
+++ interfaz/tienda/tiendaWeShop.js
@@ -54,23 +54,19 @@ window.onload = function () {
         'imgprueba6.jpg'
 
     ];
-
     const TIEMPO_INTERVALO_MILESIMAS_SEG = 1000;
     let posicionActual = 0;
-    let $botonRetroceder = document.querySelector('#retroceder');
-    let $botonAvanzar = document.querySelector('#avanzar');
     let $imagen = document.querySelector('#imagen');
-    let $botonPlay = document.querySelector('#play');
-    let $botonStop = document.querySelector('#stop');
     let intervalo;
 
+    $imagen.classList.add('img-ajustada');
     // Funciones
 
     /**
      * Funcion que cambia la foto en la siguiente posicion
      */
     function pasarFoto() {
-        if(posicionActual >= IMAGENES.length - 1) {
+        if (posicionActual >= IMAGENES.length - 1) {
             posicionActual = 0;
         } else {
             posicionActual++;
@@ -79,22 +75,10 @@ window.onload = function () {
     }
 
     /**
-     * Funcion que cambia la foto en la anterior posicion
-     */
-    function retrocederFoto() {
-        if(posicionActual <= 0) {
-            posicionActual = IMAGENES.length - 1;
-        } else {
-            posicionActual--;
-        }
-        renderizarImagen();
-    }
-
-    /**
      * Funcion que actualiza la imagen de imagen dependiendo de posicionActual
      */
-    function renderizarImagen () {
-        $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+    function renderizarImagen() {
+        $imagen.src = IMAGENES[posicionActual];
     }
 
     /**
@@ -102,31 +86,9 @@ window.onload = function () {
      */
     function playIntervalo() {
         intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
-        // Desactivamos los botones de control
-        $botonAvanzar.setAttribute('disabled', true);
-        $botonRetroceder.setAttribute('disabled', true);
-        $botonPlay.setAttribute('disabled', true);
-        $botonStop.removeAttribute('disabled');
-
     }
 
-    /**
-     * Para el autoplay de la imagen
-     */
-    function stopIntervalo() {
-        clearInterval(intervalo);
-        // Activamos los botones de control
-        $botonAvanzar.removeAttribute('disabled');
-        $botonRetroceder.removeAttribute('disabled');
-        $botonPlay.removeAttribute('disabled');
-        $botonStop.setAttribute('disabled', true);
-    }
-
-    // Eventos
-    $botonAvanzar.addEventListener('click', pasarFoto);
-    $botonRetroceder.addEventListener('click', retrocederFoto);
-    $botonPlay.addEventListener('click', playIntervalo);
-    $botonStop.addEventListener('click', stopIntervalo);
     // Iniciar
     renderizarImagen();
+    playIntervalo();
 }
